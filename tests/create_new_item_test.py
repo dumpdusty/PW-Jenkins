9083fa740913273c5081c9259390091b7b23a5e3
Fazil M <dumpdusty@gmail.com>
2025-12-09 11:54:33 -0800

    add more tests

--- tests/create_new_item_test.py
+++ tests/create_new_item_test.py
@@ -2,17 +2,26 @@ import time
 from playwright.sync_api import sync_playwright, expect
 
 new_item_name = lambda issue_name: f"test{issue_name}_{int(time.time())}"
+created_item_loc = lambda page, item_name: page.locator(f'td>a[href="job/{item_name}/"]')
 
 
-def new_item_name(issue_name):
-    return f"test_{issue_name}_{int(time.time())}"
+def test_verify_create_freestyle_project(page):
+    new_freestyle_project = new_item_name('freestyle_project')
+    
+    page.goto("/")
+    
+    page.locator('a:has-text("New Item")').click()
+    page.locator('input#name').fill(new_freestyle_project)
+    page.get_by_text("Freestyle project", exact=True).click()
+    page.locator('button#ok-button').click()
+    page.locator('.app-jenkins-logo').click()
+    
+    expect(created_item_loc(page, new_freestyle_project)).to_have_text(new_freestyle_project)    
 
 
-def test_verify_create_new_pipeline(page):
+def test_verify_create_pipeline(page):
     
     new_pipeline = new_item_name('pipeline')
-    
-    created_item_loc = page.locator(f'td>a[href="job/{new_pipeline}/"]')
     
     page.goto("/")
     
@@ -22,12 +31,24 @@ def test_verify_create_new_pipeline(page):
     page.locator('button#ok-button').click()
     page.locator('.app-jenkins-logo').click()
     
-    expect((created_item_loc)).to_have_text(new_pipeline)
+    expect((created_item_loc(page, new_pipeline))).to_have_text(new_pipeline)
+
+def test_verify_create_mC_project(page):
+    new_mf_project = new_item_name('mf_project')
+    
+    page.goto("/")
+    
+    page.locator('a:has-text("New Item")').click()
+    page.locator('input#name').fill(new_mf_project)
+    page.get_by_text("Multi-configuration project", exact=True).click()
+    page.locator('button#ok-button').click()
+    page.locator('.app-jenkins-logo').click()
+    
+    expect(created_item_loc(page, new_mf_project)).to_have_text(new_mf_project)    
     
     
-def test_verify_create_new_folder(page):
+def test_verify_create_folder(page):
     new_folder = new_item_name('folder')
-    created_item_loc = page.locator(f'td>a[href="job/{new_folder}/"]')
     
     page.goto("/")
     
@@ -37,6 +58,32 @@ def test_verify_create_new_folder(page):
     page.locator('button#ok-button').click()
     page.locator('.app-jenkins-logo').click()
     
-    expect(created_item_loc).to_have_text(new_folder)
+    expect(created_item_loc(page, new_folder)).to_have_text(new_folder)
     
+
+def test_verify_create_mb_pipeline(page):
+    new_mb_pipeline = new_item_name('mb_pipeline')
+    
+    page.goto("/")
+    
+    page.locator('a:has-text("New Item")').click()
+    page.locator('input#name').fill(new_mb_pipeline)
+    page.get_by_text("Multibranch Pipeline", exact=True).click()
+    page.locator('button#ok-button').click()
+    page.locator('.app-jenkins-logo').click()
+    
+    expect(created_item_loc(page, new_mb_pipeline)).to_have_text(new_mb_pipeline)    
    
+
+def test_verify_create_org_folder(page):
+    new_org_folder = new_item_name('org_folder')
+    
+    page.goto("/")
+    
+    page.locator('a:has-text("New Item")').click()
+    page.locator('input#name').fill(new_org_folder)
+    page.get_by_text("Organization Folder", exact=True).click()
+    page.locator('button#ok-button').click()
+    page.locator('.app-jenkins-logo').click()
+    
+    expect(created_item_loc(page, new_org_folder)).to_have_text(new_org_folder)
